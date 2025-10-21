1588a1f2c64c4bffa0a8c1c00cfed1d0fcc7734d
devawilliams02 <kyfspyrfvh@privaterelay.appleid.com>
2025-10-21 06:23:58 +0530

    Enhance main.tf with CloudFront and S3 outputs
    Added outputs for website bucket name and CloudFront details.

--- terraform/environments/devel/main.tf
+++ terraform/environments/devel/main.tf
@@ -6,6 +6,7 @@ terraform {
       version = "~> 5.0"
     }
   }
+
   backend "s3" {
     bucket         = "devawilliams-devopschallenge-terraform-state-ap-south-1"
     key            = "devel/terraform.tfstate"
@@ -19,9 +20,23 @@ provider "aws" {
   region = var.aws_region
 }
 
+# CloudFront + S3 setup using module
 module "cloudfront_s3" {
-  source      = "../../modules/cloudfront-s3"
-  environment = "devel"
+  source       = "../../modules/cloudfront-s3"
+  environment  = "devel"
   project_name = var.project_name
-  aws_region  = var.aws_region
+  aws_region   = var.aws_region
+}
+
+# Add these outputs (used by GitHub Actions)
+output "website_bucket_name" {
+  value = module.cloudfront_s3.website_bucket_name
+}
+
+output "cloudfront_distribution_id" {
+  value = module.cloudfront_s3.cloudfront_distribution_id
+}
+
+output "cloudfront_domain_name" {
+  value = module.cloudfront_s3.cloudfront_domain_name
 }
